ccb86a8dce19d4f278698e7258354010c0812302
NikVolf <nikvolf@gmail.com>
2018-02-03 02:04:18 +0300

    replace to new version

--- ethcore/wasm/src/lib.rs
+++ ethcore/wasm/src/lib.rs
@@ -16,34 +16,31 @@
 
 //! Wasm Interpreter
 
-extern crate vm;
+extern crate byteorder;
+extern crate ethcore_logger;
 extern crate ethereum_types;
 #[macro_use] extern crate log;
-extern crate ethcore_logger;
-extern crate byteorder;
-extern crate parity_wasm;
 extern crate libc;
+extern crate parity_wasm;
+extern crate vm;
 extern crate wasm_utils;
+extern crate wasmi;
 
 mod runtime;
-mod ptr;
-mod result;
 #[cfg(test)]
 mod tests;
 mod env;
 mod panic_payload;
-
-const DEFAULT_STACK_SPACE: u32 = 5 * 1024 * 1024;
-
-use parity_wasm::{interpreter, elements};
-use parity_wasm::interpreter::ModuleInstanceInterface;
+mod parser;
 
 use vm::{GasLeft, ReturnData, ActionParams};
-use self::runtime::{Runtime, RuntimeContext, UserTrap};
+use wasmi::Error as InterpreterError;
 
-pub use self::runtime::InterpreterError;
+use runtime::{Runtime, RuntimeContext};
 
-const DEFAULT_RESULT_BUFFER: usize = 1024;
+use ethereum_types::U256;
+
+const MEMORY_STIPEND: u32 = 17;
 
 /// Wrapped interpreter error
 #[derive(Debug)]
@@ -61,52 +58,42 @@ impl From<Error> for vm::Error {
 	}
 }
 
-impl From<UserTrap> for vm::Error {
-	fn from(e: UserTrap) -> Self { e.into() }
-}
-
 /// Wasm interpreter instance
-pub struct WasmInterpreter {
-	program: runtime::InterpreterProgramInstance,
-	result: Vec<u8>,
-}
+pub struct WasmInterpreter;
 
-impl WasmInterpreter {
-	/// New wasm interpreter instance
-	pub fn new() -> Result<WasmInterpreter, Error> {
-		Ok(WasmInterpreter {
-			program: interpreter::ProgramInstance::new()?,
-			result: Vec::with_capacity(DEFAULT_RESULT_BUFFER),
-		})
+impl From<runtime::Error> for vm::Error {
+	fn from(e: runtime::Error) -> Self {
+		vm::Error::Wasm(format!("Wasm runtime error: {:?}", e))
 	}
 }
 
 impl vm::Vm for WasmInterpreter {
 
 	fn exec(&mut self, params: ActionParams, ext: &mut vm::Ext) -> vm::Result<GasLeft> {
-		use parity_wasm::elements::Deserialize;
+		let (module, data) = parser::payload(&params, ext.schedule())?;
 
-		let code = params.code.expect("exec is only called on contract with code; qed");
+		let loaded_module = wasmi::Module::from_parity_wasm_module(module).map_err(Error)?;
 
-		trace!(target: "wasm", "Started wasm interpreter with code.len={:?}", code.len());
+		let instantiation_resolover = env::ImportResolver::with_limit(64);
 
-		let env_instance = self.program.module("env")
-			// prefer explicit panic here
-			.expect("Wasm program to contain env module");
-
-		let env_memory = env_instance.memory(interpreter::ItemIndex::Internal(0))
-			// prefer explicit panic here
-			.expect("Linear memory to exist in wasm runtime");
+		let module_instance = wasmi::ModuleInstance::new(
+			&loaded_module,
+			&wasmi::ImportsBuilder::new().with_resolver("env", &instantiation_resolover)
+		).map_err(Error)?;
 
 		if params.gas > ::std::u64::MAX.into() {
 			return Err(vm::Error::Wasm("Wasm interpreter cannot run contracts with gas >= 2^64".to_owned()));
 		}
 
+		let initial_memory = instantiation_resolover.memory_size().map_err(Error)?
+			.saturating_sub(MEMORY_STIPEND);
+		trace!(target: "wasm", "Contract requested {:?} pages of initial memory", initial_memory);
+
 		let mut runtime = Runtime::with_params(
 			ext,
-			env_memory,
-			DEFAULT_STACK_SPACE,
+			instantiation_resolover.memory_ref().map_err(Error)?,
 			params.gas.low_u64(),
+			data.to_vec(),
 			RuntimeContext {
 				address: params.address,
 				sender: params.sender,
@@ -114,86 +101,54 @@ impl vm::Vm for WasmInterpreter {
 				code_address: params.code_address,
 				value: params.value.value(),
 			},
-			&self.program,
 		);
 
-		let (mut cursor, data_position) = match params.params_type {
-			vm::ParamsType::Embedded => {
-				let module_size = parity_wasm::peek_size(&*code);
-				(
-					::std::io::Cursor::new(&code[..module_size]),
-					module_size
-				)
-			},
-			vm::ParamsType::Separate => {
-				(::std::io::Cursor::new(&code[..]), 0)
-			},
-		};
+		// cannot overflow if static_region < 2^16,
+		// initial_memory ∈ [0..2^32)
+		// total_charge <- static_region * 2^32 * 2^16
+		// total_charge ∈ [0..2^64) if static_region ∈ [0..2^16)
+		// qed
+		assert!(runtime.schedule().wasm.static_region < 2^16);
+		runtime.charge(|s| initial_memory as u64 * 64 * 1024 * s.wasm.static_region as u64)?;
 
-		let contract_module = wasm_utils::inject_gas_counter(
-			elements::Module::deserialize(
-				&mut cursor
-			).map_err(|err| {
-				vm::Error::Wasm(format!("Error deserializing contract code ({:?})", err))
-			})?,
-			runtime.gas_rules(),
-		);
+		let module_instance = module_instance.run_start(&mut runtime).map_err(Error)?;
 
-		let data_section_length = contract_module.data_section()
-			.map(|section| section.entries().iter().fold(0, |sum, entry| sum + entry.value().len()))
-			.unwrap_or(0)
-			as u64;
-
-		let static_segment_cost = data_section_length * runtime.ext().schedule().wasm.static_region as u64;
-		runtime.charge(|_| static_segment_cost).map_err(Error)?;
-
-		let d_ptr = {
-			match params.params_type {
-				vm::ParamsType::Embedded => {
-					runtime.write_descriptor(
-						if data_position < code.len() { &code[data_position..] } else { &[] }
-					).map_err(Error)?
-				},
-				vm::ParamsType::Separate => {
-					runtime.write_descriptor(&params.data.unwrap_or_default())
-						.map_err(Error)?
+		match module_instance.invoke_export("call", &[], &mut runtime) {
+			Ok(_) => { },
+			Err(InterpreterError::Host(boxed)) => {
+				match boxed.downcast_ref::<runtime::Error>() {
+					None => {
+						return Err(vm::Error::Wasm("Invalid user error used in interpreter".to_owned()));
+					}
+					Some(runtime_err) => {
+						match *runtime_err {
+							runtime::Error::Suicide => {
+								// Suicide uses trap to break execution
+							}
+							ref any_err => {
+								trace!(target: "wasm", "Error executing contract: {:?}", boxed);
+								return Err(vm::Error::from(Error::from(InterpreterError::Host(Box::new(any_err.clone())))));
+							}
+						}
+					}
 				}
-			}
-		};
-
-		{
-			let execution_params = runtime.execution_params()
-				.add_argument(interpreter::RuntimeValue::I32(d_ptr.as_raw() as i32));
-
-			let module_instance = self.program.add_module("contract", contract_module, Some(&execution_params.externals))
-				.map_err(|err| {
-					trace!(target: "wasm", "Error adding contract module: {:?}", err);
-					vm::Error::from(Error(err))
-				})?;
-
-			match module_instance.execute_export("_call", execution_params) {
-				Ok(_) => { },
-				Err(interpreter::Error::User(UserTrap::Suicide)) => { },
-				Err(err) => {
-					trace!(target: "wasm", "Error executing contract: {:?}", err);
-					return Err(vm::Error::from(Error(err)))
-				}
+			},
+			Err(err) => {
+				trace!(target: "wasm", "Error executing contract: {:?}", err);
+				return Err(vm::Error::from(Error::from(err)))
 			}
 		}
 
-		let result = result::WasmResult::new(d_ptr);
-		if result.peek_empty(&*runtime.memory()).map_err(|e| Error(e))? {
+		if runtime.result().is_empty() {
 			trace!(target: "wasm", "Contract execution result is empty.");
 			Ok(GasLeft::Known(runtime.gas_left()?.into()))
 		} else {
-			self.result.clear();
-			// todo: use memory views to avoid copy
-			self.result.extend(result.pop(&*runtime.memory()).map_err(|e| Error(e.into()))?);
-			let len = self.result.len();
+			let len = runtime.result().len();
+			let gas_left: U256 = runtime.gas_left()?.into();
 			Ok(GasLeft::NeedsReturn {
-				gas_left: runtime.gas_left().map_err(|e| Error(e.into()))?.into(),
+				gas_left: gas_left,
 				data: ReturnData::new(
-					::std::mem::replace(&mut self.result, Vec::with_capacity(DEFAULT_RESULT_BUFFER)),
+					runtime.dissolve(),
 					0,
 					len,
 				),
